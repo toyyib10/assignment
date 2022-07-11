194ef42d674915e8f554b2ff87daebb341794ddd
Yekeen Toyyib <yekeentoyyib@gmail.com>
2022-07-11 09:01:17 +0100

    i added an index file

--- weatherapp/weather.js
+++ weatherapp/weather.js
@@ -7,7 +7,7 @@
    let m;
    let s;
    let amPm;
-   function loadData(){
+   let loadData = ()=>{
      navigator.geolocation.getCurrentPosition(
         async (position)=>{
           lat = position.coords.latitude;
@@ -15,7 +15,6 @@
           const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=1475f9350b35a7b2e9833bcba802614f&units=metric`;
           let response = await fetch(url);
           convertedResponse = await response.json();
-          console.log(convertedResponse)
           cit.value = convertedResponse.name;
           var val = convertedResponse.main.temp;
           temp.value = val.toFixed(2)
@@ -24,7 +23,7 @@
           var feel = convertedResponse.main.feels_like
           var humid = convertedResponse.main.humidity;
           var windSpeed = convertedResponse.wind.speed;
-          humidity.innerHTML = `Feels Like : ${feel}  <sup style="font-size:0.6em; font-family:'Trebuchet MS', 'Lucida Sans Unicode','Lucida Grande', 'Lucida Sans',Arial, sans-serif;">o</sup><span style="font-size:1em;margin-top:20px; font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans',Arial, sans-serif;">C</span> <br> Humidity : ${humid}% <br> Wind Speed : ${windSpeed} m/s`
+          humidity.innerHTML = `Feels Like : ${feel}  <sup class="o">o</sup><span class="c">C</span> <br> Humidity : ${humid}% <br> Wind Speed : ${windSpeed} m/s`
           const url2 =  `https://api.ipgeolocation.io/timezone?apiKey=680cc1c7a6f645ae893d6c578ecb1ac9&lat=${lat}&lon=${long}` 
           let response2 = await fetch(url2);   
           convertedResponse2 = await response2.json();
@@ -34,31 +33,30 @@
           amPm = time.split(':');
           [h , m, s] = amPm;
           if (h <= 12){
-            da.innerHTML = `<span style="font-size: 4em; font-family:'Trebuchet MS','Lucida Sans Unicode','Lucida Grande','Lucida Sans',Arial,sans-serif;">${time}</span> <span style="font-size:2em;  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;">AM</span>`
+            da.innerHTML = `<span class="tim">${time}</span> <span class="amPm">AM</span>`
           }
           else {
-            da.innerHTML = `<span style="font-size: 4em; font-family:'Trebuchet MS','Lucida Sans Unicode','Lucida Grande','Lucida Sans',Arial,sans-serif;">${time}</span> <span style="font-size:2em;  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;">PM</span>`
+            da.innerHTML = `<span class="tim">${time}</span> <span class="amPm">PM</span>`
           }
           dy.innerText = ` ${day} ${date}`
         }
       ) 
     }
-    async function getData(){
+    let getData = async ()=>{
       city = cit.value;
       const url2 =  `https://api.ipgeolocation.io/timezone?apiKey=680cc1c7a6f645ae893d6c578ecb1ac9&location=${city}` 
       let response2 = await fetch(url2)   
       convertedResponse2 = await response2.json()
-      //console.log(convertedResponse2.date_time_txt.split(' '))
       dateArray = convertedResponse2.date_time_txt.split(',')
       let [day , date, dat] = dateArray
       let [ , , time] = dat.split(' ')
       amPm = time.split(':')
       [h ,m , s] = amPm
       if (h <= 12){
-        da.innerHTML = `<span style="font-size: 4em; font-family:'Trebuchet MS','Lucida Sans Unicode','Lucida Grande','Lucida Sans',Arial,sans-serif;">${time}</span> <span style="font-size:2em;  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif; ">AM</span>`
+        da.innerHTML = `<span class="tim">${time}</span> <span class="amPm">AM</span>`
       }
       else {
-        da.innerHTML = `<span style="font-size: 4em; font-family:'Trebuchet MS','Lucida Sans Unicode','Lucida Grande','Lucida Sans',Arial,sans-serif;">${time}</span> <span style="font-size:2em;  font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;">PM</span>`
+        da.innerHTML = `<span class="tim">${time}</span> <span class="amPm">PM</span>`
       }
       dy.innerText = ` ${day} ${date}`
       const url =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1475f9350b35a7b2e9833bcba802614f&units=metric`
@@ -70,5 +68,5 @@
       des.innerText = description;
       var feel = convertedResponse.main.feels_like
       var humid = convertedResponse.main.humidity
-      humidity.innerHTML = `Feels Like : ${feel}  <sup style="font-size:0.6em; font-family:'Trebuchet MS', 'Lucida Sans Unicode','Lucida Grande', 'Lucida Sans',Arial, sans-serif;">o</sup><span style="font-size:1em;margin-top:20px; font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans',Arial, sans-serif;">C</span> <br> Humidity : ${humid}%`
+      humidity.innerHTML = `Feels Like : ${feel}  <sup class="o">o</sup><span class="c">C</span> <br> Humidity : ${humid}% <br> Wind Speed : ${windSpeed} m/s`
     }
